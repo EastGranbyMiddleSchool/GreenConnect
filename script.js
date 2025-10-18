e06b68b79ff27bf4560828683d6205ce8791cc1e
EastGranbyMiddleSchool <s0n1k.sof11a.a@gmail.com>
2025-10-18 17:23:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-    // Ініціалізація карти
     var map = L.map('map').setView([41.6, -72.7], 10);
-
-    // Базовий шар OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; OpenStreetMap contributors'
     }).addTo(map);
 
-    // Тестові точки
     var locations = [
-        {name: "East Granby Recycling ♻️", coords: [41.8792, -72.7382], type: "Recycling"},
-        {name: "Hartford Park 🌳", coords: [41.7658, -72.6734], type: "Park"},
-        {name: "Windsor Locks Community Garden 🌻", coords: [41.9223, -72.6270], type: "Garden"},
-        {name: "West Hartford Recycling ♻️", coords: [41.7663, -72.7421], type: "Recycling"},
-        {name: "Simsbury Park 🌳", coords: [41.8430, -72.8418], type: "Park"}
+        {name: "East Granby Recycling ♻️", coords: [41.8792, -72.7382], info: "East Granby recycling center info"},
+        {name: "Hartford Park 🌳", coords: [41.7658, -72.6734], info: "Hartford Park details"},
+        {name: "Windsor Locks Community Garden 🌻", coords: [41.9223, -72.6270], info: "Windsor Locks Garden info"}
     ];
 
-    // Додаємо точки на карту
     locations.forEach(function(loc){
-        L.marker(loc.coords).addTo(map)
-         .bindPopup("<b>" + loc.name + "</b><br>Type: " + loc.type);
+        var marker = L.marker(loc.coords).addTo(map).bindPopup(loc.name);
+        marker.on('click', function() {
+            document.getElementById('info-panel').innerText = loc.info;
+        });
     });
-
 });
